c288336bda276998bab478277e2a2ec6497f044e
Ali Sinan Köksal <alisinan@gmail.com>
2016-10-05 00:39:25 -0700

    minor

--- src/main/scala/funchisqwrapper/discretization/CkmeansInterface.scala
+++ src/main/scala/funchisqwrapper/discretization/CkmeansInterface.scala
@@ -1,7 +1,12 @@
 package funchisqwrapper.discretization
 
+import org.ddahl.rscala.RClient
+
 object CkmeansInterface {
-  def ckmeans() = {
+  def ckmeans(xs: Seq[Double]): Seq[Int] = {
+    // TODO consider moving to preprocess
+    val R = RClient()
+
 
   }
 }
